fcfb32f1fa3df18b7a435bc9a849b16eec461e60
jlukasiu <jedrzej.lukasiuk@gmail.com>
2018-04-29 22:42:25 +0200

    Refactored script. Still doesn't work as expected but progress is visible

--- server/tensor/run.py
+++ server/tensor/run.py
@@ -1,105 +1,140 @@
-import cv2
-import sys
 import numpy as np
+import sys
+import cv2
 import math
 import random
+import matplotlib.pyplot as plt
 
-DEV_MODE = 1
-B_CHANNEL, G_CHANNEL, R_CHANNEL = (0,1,2)
+class ImagePainter:
+    R_CHANNEL, G_CHANNEL, B_CHANNEL = (0, 1, 2)
+    DEV_MODE = 0
 
-def getArgs():
-    img, N, SAMPLES = (None, 5, 1000)
-    if len(sys.argv) == 4:
-        img = sys.argv[1] 
-        N = sys.argv[2]
-        SAMPLES = sys.argv[3]
-    return img, N, SAMPLES
+    def __init__(self, color, gray, N):
+        self.colorImg = cv2.cvtColor(cv2.imread(color), cv2.COLOR_BGR2RGB)
+        self.grayImg = cv2.imread(gray)
+        grayHeight, grayWidth, grayChannels = self.grayImg.shape
+        if grayChannels == 3:
+            self.grayImg = cv2.cvtColor(self.grayImg, cv2.COLOR_BGR2GRAY)
+        self.outImg = np.zeros([grayHeight, grayWidth, 3])
+        self.blurImg = None
+        self.clusters = N
+        self.estimatedColors = np.zeros([self.clusters,3])
+        colorHeight, colorWidth = (self.colorImg.shape[0], self.colorImg.shape[1])
+        self.samples = int(0.1 * colorHeight * colorWidth)
+        self.colorThresholds = np.zeros(self.clusters)
+        self.grayThresholds = np.zeros(self.clusters)
 
-img, N, SAMPLES = getArgs()
+    def findColorThresholds(self, useBlur=False):
+        heigth, width = (self.colorImg.shape[0], self.colorImg.shape[1])
+        processingImage = self.colorImg
+        if useBlur:
+            processingImage = cv2.blur(self.colorImg, (5, 5))
+            self.blurImg = processingImage
+        cumHist = np.zeros([heigth, width])
+        for x in range(0, heigth):
+            for y in range(0, width):
+                R_value = 256**2 * int(processingImage[x, y, self.B_CHANNEL])
+                G_value = 256 * int(processingImage[x, y, self.G_CHANNEL])
+                B_value = int(processingImage[x, y, self.R_CHANNEL])
+                cumHist[[x], [y]] = R_value + B_value + G_value
 
-img_color = cv2.imread(img if img else 'black_hotel.jpg')
-img_gray = cv2.cvtColor(img_color, cv2.COLOR_BGR2GRAY)
-heigth, width, channels = img_color.shape
-if DEV_MODE:
-    print(heigth, width, channels)
+        cumHistVecSort = np.sort(cumHist.flatten())
+        self.colorThresholds = self.calculateColorIndividuals(cumHistVecSort)
+        if self.DEV_MODE:
+            print('color thresholds {}'.format(self.colorThresholds))
+    
+    # think about minimal variance spread
+    def calculateColorIndividuals(self, sortedHist, useMean=False):
+        spread = int(len(sortedHist) / self.clusters) 
+        thresholds = np.zeros(self.clusters)
+        for i in range(0, self.clusters):
+            lowerBand = i * spread
+            upperBand = (i+1) * spread
+            if useMean:
+                thresholds[i] = int(np.mean(sortedHist[lowerBand:upperBand]))
+            else:
+                thresholds[i] = np.median(sortedHist[lowerBand:upperBand])
+        return thresholds
+    
+    # randomly pick samples to estimate color being equivalent to threshold - quasi Monte Carlo method
+    def findColorTriples(self):
+        img = self.blurImg if self.blurImg != None else self.colorImg
+        heigth, width = (img.shape[0], img.shape[1])
+        # r,g,b + clusterId
+        classifiedPixels = np.zeros((self.samples, 4)) 
+        for sample in range(0, self.samples):
+            r = img[random.randint(0, heigth - 1), random.randint(0, width - 1), self.R_CHANNEL]
+            b = img[random.randint(0, heigth - 1), random.randint(0, width - 1), self.B_CHANNEL]
+            g = img[random.randint(0, heigth - 1), random.randint(0, width - 1), self.G_CHANNEL]
+            color = int(256**2 * r) + int(256 * g) + int(b)
+            clusterId = np.argmin([abs(threshold - color) for threshold in self.colorThresholds ])
+            classifiedPixels[(sample)] = [r,g,b,clusterId]
 
-blur = cv2.blur(img_color, (5, 5))
-cumHist = np.zeros([heigth, width])
+        self.estimatedColors = self.findColorIndividuals(classifiedPixels)
+        if self.DEV_MODE:
+            # print(classifiedPixels)
+            print('estimated Colors: \n{}'.format(self.estimatedColors))
 
-for x in range(0, heigth):
-    for y in range(0, width):
-        cumHist[[x], [y]] = (int(blur[x, y, B_CHANNEL]) +
-                             int(blur[x, y, G_CHANNEL]) + int(blur[x, y, R_CHANNEL]))
+    def findColorIndividuals(self, classifiedPixels, useMean=False):
+        CLUSTER_INDEX = 3
+        colors = np.zeros([self.clusters,3])
+        for index in range(0, self.clusters):
+            if useMean:
+                triple = np.mean(classifiedPixels[classifiedPixels[:, CLUSTER_INDEX] == index], axis=0)
+            else:
+                triple = np.median(classifiedPixels[classifiedPixels[:, CLUSTER_INDEX] == index], axis=0)
+            colors[(index)] = triple[:3]
+        return colors
 
-cumHistVec = cumHist.flatten()
-cumHistVecSort = np.sort(cumHistVec)
+    def colorGrayImage(self, save=True):
+        height, width = (self.grayImg.shape[0], self.grayImg.shape[1])
+        outImg = np.zeros([height, width, 3])
+        # intensity = 0.2989*RED + 0.5870*GREEN + 0.1140*BLUE 
+        file = open('results.txt','w')
+        for x in range(0,height):
+            for y in range(0,width):
+                intensity = self.grayImg[x,y]
+                file.write("intensity {}\t".format(intensity))
+                idx = None
+                diffs = np.zeros(self.clusters)
+                index = 0
+                for color in self.estimatedColors:
+                    diff = abs(intensity -  (int(0.29 * color[0]) + int(0.58 * color[1]) + int(0.11 * color[2])))
+                    diffs[index] = diff
+                    index = index + 1
+                idx = np.argmin(diffs)
+                file.write("class{}\t".format(idx))
+                file.write("diffs\t")
+                diffs.tofile(file,sep=", ")
+                file.write("\n")
+                # idx = np.argmin([abs(intensity - (0.29 * pixel[0] + 0.58 * pixel[1] + 0.11 * pixel[2])) for pixel in self.estimatedColors])
+                outImg[[x], [y], self.B_CHANNEL] = self.estimatedColors[idx, 0]
+                outImg[[x], [y], self.G_CHANNEL] = self.estimatedColors[idx, 1]
+                outImg[[x], [y], self.R_CHANNEL] = self.estimatedColors[idx, 2]
+        file.close()
+        self.outImg = outImg
+        if save:
+            cv2.imwrite('colored.jpg', self.outImg)
+            print("image saved")
+                
+def main(**kwargs):
+    inputColor, inputGray, clusters = (None, None, 5)
+    for key, value in kwargs.items():
+        if key == 'inputColor':
+            inputColor = value
+        elif key == 'inputGray':
+            inputGray = value
+        elif key == '':
+            clusters = value
+    ImagePainter.DEV_MODE = 1
+    imagePainter = ImagePainter(inputColor, inputGray, clusters)
+    imagePainter.findColorThresholds()
+    imagePainter.findColorTriples()
+    imagePainter.colorGrayImage()
 
-thresholds = np.zeros((1, N))
-spread = int(len(cumHistVec) / N)
 
-for i in range(0, N):
-    lowerBand = i * spread
-    upperBand = (i+1) * spread
-    thresholds[(0, i)] = np.median(cumHistVecSort[lowerBand:upperBand])
-
-if DEV_MODE:
-    print(thresholds)
-
-pixelClusters = np.zeros((SAMPLES, 4))
-print(pixelClusters)
-
-for sample in range(0, SAMPLES):
-    b = blur[random.randint(0, heigth - 1),
-             random.randint(0, width - 1), B_CHANNEL]
-    g = blur[random.randint(0, heigth - 1),
-             random.randint(0, width - 1), G_CHANNEL]
-    r = blur[random.randint(0, heigth - 1),
-             random.randint(0, width - 1), R_CHANNEL]
-    sum = int(b) + int(g) + int(r)
-    differs = np.zeros((1, N))
-    for clusterIdx in range(0, N):
-        differs[(0, clusterIdx)] = abs(thresholds[(0, clusterIdx)] - sum)
-    idx = np.argmin(differs)
-    pixelClusters[(sample, 0)] = b
-    pixelClusters[(sample, 1)] = g
-    pixelClusters[(sample, 2)] = r
-    pixelClusters[(sample, 3)] = idx
-
-# sortedPixelClusters = pixelClusters[pixelClusters[:, 3].argsort()]
-triples = np.zeros((N, 3))
-for ind in range(0, N):
-    triple = np.median(pixelClusters[pixelClusters[:, 3] == ind], axis=0)
-    triples[(ind, 0)] = triple[0]
-    triples[(ind, 1)] = triple[1]
-    triples[(ind, 2)] = triple[2]
-
-
-if DEV_MODE:
-    print(triples)
-    print(triples[0,0])
-    print(triples[0,1])
-    print(triples[0,2])
-
-triples_mean = [int(np.mean(row)) for row in triples]
-if DEV_MODE:
-    print(triples_mean)
-
-out = np.zeros([heigth, width, 3])
-
-# count hist
-grayHist = img_gray.flatten()
-# cv2.calcHist()
-grayHistSort = np.sort(grayHist)
-
-arr = [1, 100, 88, 140, 222]
-for a in arr:
-    print(np.argmin([abs(a - mean) for mean in triples_mean]))
-
-for x in range(0, heigth):
-    for y in range(0, width):
-        idx = np.argmin([abs(img_gray[x,y] - mean) for mean in triples_mean])
-        out[[x],[y],0] = triples[idx,0]
-        out[[x],[y],1] = triples[idx,1]
-        out[[x],[y],2] = triples[idx,2]
-
-cv2.imwrite('test.png',out)
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        raise SyntaxError("Insufficient arguments")
+    else:
+        main(**dict(arg.split('=') for arg in sys.argv[1:]))
