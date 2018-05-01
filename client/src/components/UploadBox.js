d033af5ce589ee957e2dca4e297e75db1f437770
jlukasiu <jedrzej.lukasiuk@gmail.com>
2018-05-01 16:06:16 +0200

    fixed proxy issue

--- client/src/components/UploadBox.js
+++ client/src/components/UploadBox.js
@@ -66,10 +66,7 @@ export default class UploadBox extends Component {
     );
     fileName = `${fileName}${extension}`;
     fd.append(fileName, file);
-    const url =
-      process.env.NODE_ENV === "production"
-        ? "upload"
-        : "http://localhost:5000/upload";
+    const url = "/upload";
     axios
       .post(url, fd)
       .then(res => {
