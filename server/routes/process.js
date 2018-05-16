57e182d64dcfeac1cd33ce20e62b8041492031cd
jlukasiu <jedrzej.lukasiuk@gmail.com>
2018-05-16 20:08:57 +0200

    temporary placeholder due to docker problems...

--- server/routes/process.js
+++ server/routes/process.js
@@ -3,10 +3,18 @@ const fs = require("fs");
 const router = express.Router();
 const path = require("path");
 const { processImage } = require("../tensor/processingModule");
+const DOCKER_IS_NOT_WORKING = 1;
 
 router.get("/process", (req, res) => {
-  const { grayImg, colorImg, outImg } = req.query;
-  console.log(`Parameters requested: ${grayImg} , ${colorImg} , ${outImg}`);
+  if (DOCKER_IS_NOT_WORKING) {
+    setTimeout(() => {
+      res.send({
+        outImg: 'img1.jpg'
+      })
+    }, 5000);
+  } else {
+    const { grayImg, colorImg, outImg } = req.query;
+    console.log(`Parameters requested: ${grayImg} , ${colorImg} , ${outImg}`);
     processImage(grayImg, colorImg, outImg)
       .then(data => {
         res.send(data);
@@ -14,6 +22,7 @@ router.get("/process", (req, res) => {
       .catch(err => {
         res.send(err);
       });
+  }
 });
 
 module.exports = router;
