18ddb06df6fc06ad940a4b38da87eda1f4020969
jlukasiu <jedrzej.lukasiuk@gmail.com>
2018-07-03 12:22:14 +0200

    added config files

---
+++ server/config/paths.js
@@ -0,0 +1,26 @@
+const Promise = require('bluebird').Promise;
+const fs = require("fs");
+const path = require("path");
+const HERE = __dirname;
+
+const checkPaths = () => {
+  const assetsDir = path.join(HERE, "..", "uploads");
+  // const readyImgDir = path.join(assetsDir, "ready");
+  if (!fs.existsSync(assetsDir)) {
+    createDir(assetsDir, "Could not create assets directory");
+    // createDir(readyImgDir, "Could not create assets directory");
+  } 
+  // else if (!fs.existsSync(readyImgDir)) {
+  //   createDir(readyImgDir, "Could not create assets directory");
+  // }
+};
+
+const createDir = (dir, errorMsg) => {
+  fs.mkdir(dir, err => {
+    if (err) {
+      console.log(errorMsg);
+    }
+  });
+};
+
+module.exports = checkPaths;
