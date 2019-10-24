55b909bf44ac917f966c8035a66adcec53cdd049
Jędrzej Łukasiuk <jlukasiuk@teonite.com>
2019-10-24 20:02:08 +0200

    clean up socket related actions

--- client/src/redux/actions/img.js
+++ client/src/redux/actions/img.js
@@ -5,38 +5,33 @@ import {
   requestedImageSelector,
   previousImagesSelector
 } from "../selectors/img";
+import ACTIONS from "./types";
 import io from "socket.io-client";
 import uuid from "uuid/v1";
 
 const { NODE_ENV, SOCKET_PORT = 5000 } = process.env;
-
 const worker = NODE_ENV === "development" ? `http://localhost:${SOCKET_PORT}` : "";
-export const socket = io(worker);
+const socket = io(worker);
 
-// sometimes connection hangs while processing -
-// this is why we check for ready image existence after connect
-socket.on("connect", () => {
+socket.on("response", data => {
+  const { status, msg } = data;
+  if (status === "OK") {
+    store.dispatch(setRequestedImageAvailable());
+  } else {
+    //TODO: show error placeholder to the user
+    console.error(msg);
+  }
+});
+
+socket.on("reconnect", () => {
+  // sometimes connection hangs while processing -
+  // this is why we check for ready image existence while reconnect
   const imageAvailable = requestedImageAvailableSelector(store.getState());
   const requestedImage = requestedImageSelector(store.getState());
   const previousImages = previousImagesSelector(store.getState());
   if (requestedImage && !imageAvailable && !previousImages.includes(requestedImage)) {
     socket.emit("check img", requestedImage);
-    socket.on("response", data => {
-      const { status } = data;
-      if (status === "OK") {
-        store.dispatch(setRequestedImageAvailable());
-      }
-    });
   }
-});
-
-export const ACTIONS = Object.freeze({
-  SET_GRAY: "SET_GRAY",
-  SET_COLOR: "SET_COLOR",
-  SET_REQUESTED_IMAGE: "SET_REQUESTED_IMAGE",
-  SET_REQUESTED_IMAGE_AVAILABLE: "SET_IMAGE_AVAILABLE",
-  RESET: "RESET",
-  PROCESS: "PROCESS"
 });
 
 export const setGrayImage = name => ({
@@ -71,13 +66,5 @@ export const processImage = () => (dispatch, getState) => {
     inputColor: colorImgSelector(getState()),
     outImg,
     N
-  });
-  socket.on("response", data => {
-    const { status, msg } = data;
-    if (status === "OK") {
-      dispatch(setRequestedImageAvailable());
-    } else {
-      console.error(msg);
-    }
   });
 };
