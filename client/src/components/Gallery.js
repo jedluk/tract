e8f6b3cd3a63aa8b8026f19640438a82cfdec1f9
Jedrzej Lukasiuk <jlukasiuk@teonite.com>
2019-10-29 21:27:48 +0100

    possibility to download a file

--- client/src/components/Gallery.js
+++ client/src/components/Gallery.js
@@ -1,19 +1,20 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { Link } from "react-router-dom";
-import { reset } from "../redux/actions/img";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { reset } from '../redux/actions/img';
+import FontAwesome from 'react-fontawesome';
 import {
   previousImagesSelector,
   requestedImageAvailableSelector,
   requestedImageSelector
-} from "../redux/selectors/img";
-import { getSamples, FILE_SERVER } from "../services/fileServer";
+} from '../redux/selectors/img';
+import { getSamples, FILE_SERVER } from '../services/fileServer';
 
 class Gallery extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      mainImg: "",
+      mainImg: '',
       sampleImages: []
     };
     this.mainImgRef = React.createRef();
@@ -28,14 +29,14 @@ class Gallery extends Component {
           (imgAvailable && readyImg) || sample[Math.ceil(Math.random() * sample.length - 1)];
         this.setState({ mainImg, sampleImages });
       })
-      .catch(err => console.error("cannot get sample images"));
+      .catch(err => console.error('cannot get sample images'));
   }
 
   changeMain = mainImg => {
     this.setState({ mainImg });
     const { current } = this.mainImgRef;
-    current.classList.add("fade-in");
-    setTimeout(() => current.classList.remove("fade-in"), 500);
+    current.classList.add('fade-in');
+    setTimeout(() => current.classList.remove('fade-in'), 500);
   };
 
   render() {
@@ -44,6 +45,11 @@ class Gallery extends Component {
         <div className="container">
           <div className="main-img">
             <img ref={this.mainImgRef} alt="main" src={`${FILE_SERVER}/${this.state.mainImg}`} />
+            {!!this.state.mainImg && (
+              <a href={`${FILE_SERVER}/${this.state.mainImg}`} download={this.state.mainImg}>
+                <FontAwesome name="fas fa-download" className="main-img__download-icon" size="5x" />
+              </a>
+            )}
           </div>
           <div className="imgs">
             {this.state.sampleImages.map(imgName => (
@@ -60,7 +66,7 @@ class Gallery extends Component {
           <div className="goHome">
             go <Link to="/">home</Link>
           </div>
-          Bare awesome free images are taken from{" "}
+          Bare awesome free images are taken from{' '}
           <a href="https://www.pexels.com/" rel="noopener noreferrer" target="_blank">
             pexels
           </a>
