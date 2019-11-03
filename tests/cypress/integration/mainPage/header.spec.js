f4486b8c06c720a2996f833e985b8e4b0fb35ffd
Jedrzej Lukasiuk <jlukasiuk@teonite.com>
2019-11-03 17:17:36 +0100

    added run command for tests, updates other tests

--- tests/cypress/integration/mainPage/header.spec.js
+++ tests/cypress/integration/mainPage/header.spec.js
@@ -7,12 +7,14 @@ context('Header', () => {
   });
   it('should display title correctly', () => {
     const mainPage = new MainPage();
-    mainPage()
+    mainPage
+      .header()
       .find('h1')
       .should('be.visible')
       .should('have.text', 'Image rectification');
 
-    mainPage()
+    mainPage
+      .header()
       .find('h2')
       .should('be.visible')
       .should('have.text', 'Refresh appearance of your images in 3 simple steps !');
